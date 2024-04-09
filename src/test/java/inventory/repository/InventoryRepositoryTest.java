b2ae4cc1bd55e1521efa822a382b2b1ed62636c9
Ichim Stefan <stefanichim2201@gmail.com>
2024-04-09 14:37:12 +0300

    rename search variable, Excel update

--- src/test/java/inventory/repository/InventoryRepositoryTest.java
+++ src/test/java/inventory/repository/InventoryRepositoryTest.java
@@ -113,31 +113,31 @@ class InventoryRepositoryTest {
 
     @Test
     void lookupPart_TC01(){
-        String searchString = "Cucaracia";
+        String searchItem = "Cucaracia";
         InventoryRepository repo = new InventoryRepository();
-        Part part = repo.lookupPart(searchString);
+        Part part = repo.lookupPart(searchItem);
         assert(part == null);
     }
     @Test
     void lookupPart_TC02(){
-        String searchString = "ZIMBABUE";
+        String searchItem = "ZIMBABUE";
         InventoryRepository repo = new InventoryRepository();
-        Part part = repo.lookupPart(searchString);
-        assert(part.getName().equals(searchString));
+        Part part = repo.lookupPart(searchItem);
+        assert(part.getName().equals(searchItem));
     }
     @Test
     void lookupPart_TC03(){
-        String searchString = "1";
+        String searchItem = "1";
         InventoryRepository repo = new InventoryRepository();
-        Part part = repo.lookupPart(searchString);
-        assert(part.getPartId() == Integer.parseInt(searchString));
+        Part part = repo.lookupPart(searchItem);
+        assert(part.getPartId() == Integer.parseInt(searchItem));
     }
     @Test
     void lookupPart_TC04(){
-        String searchString = "gugu";
+        String searchItem = "gugu";
         String filename = "data/empty_items.txt";
         InventoryRepository repo = new InventoryRepository(filename);
-        Part part = repo.lookupPart(searchString);
+        Part part = repo.lookupPart(searchItem);
         assert(part == null);
     }
 }
